5f09ff71743c433b37d0cb93961e840bd77515c0
Enrique Villar <9202686@alumnos.ufv.es>
2025-01-15 16:55:04 +0100

    Add Vehicle class and update MainView to display vehicle data in a grid

--- Prueba1Frontend/src/main/java/org/vaadin/example/MainView.java
+++ Prueba1Frontend/src/main/java/org/vaadin/example/MainView.java
@@ -3,10 +3,17 @@ package org.vaadin.example;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -31,30 +38,22 @@ public class MainView extends VerticalLayout {
      *
      * @param service
      *            The message service. Automatically injected Spring managed bean.
+     * @throws InterruptedException 
+     * @throws IOException 
+     * @throws URISyntaxException 
      */
-    public MainView(@Autowired GreetService service) {
+    public MainView(@Autowired GreetService service) throws URISyntaxException, IOException, InterruptedException {
+        
+        Grid<Vehicle> grid = new Grid<>(Vehicle.class, false);
+        grid.addColumn(Vehicle::getLicensePlate).setHeader("License Plate");
+        grid.addColumn(Vehicle::getMake).setHeader("Make");
+        grid.addColumn(Vehicle::getModel).setHeader("Model");
+        grid.addColumn(Vehicle::getYear).setHeader("Year");
+        grid.addColumn(Vehicle::getType).setHeader("Type");
 
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
-        textField.addClassName("bordered");
+        ArrayList<Vehicle> vehicle = DataService.getVehicles();
+        grid.setItems(vehicle);
 
-        // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello", e -> {
-            add(new Paragraph(service.greet(textField.getValue())));
-        });
-
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button has a more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
-
-        // Use custom CSS classes to apply styling. This is defined in
-        // styles.css.
-        addClassName("centered-content");
-
-        add(textField, button);
+        add(grid);
     }
 }
---
+++ Prueba1Frontend/src/main/java/org/vaadin/example/Vehicle.java
@@ -0,0 +1,73 @@
+package org.vaadin.example;
+
+import java.util.UUID;
+
+public class Vehicle {
+
+    private String make;
+    private String model;
+    private int year;
+    private String type;
+    private String licensePlate;
+    private String id;
+
+    public Vehicle(String make, String model, int year, String type, String licensePlate) {
+        this.id = UUID.randomUUID().toString();
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.type = type;
+        this.licensePlate = licensePlate;
+    }
+
+
+    
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id.toString();
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+}
