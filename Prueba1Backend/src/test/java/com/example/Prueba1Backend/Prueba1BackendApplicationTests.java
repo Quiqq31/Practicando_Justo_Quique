f8f5497c0f62b2fcc1aa470a432240bb7db32db1
Enrique Villar <9202686@alumnos.ufv.es>
2025-01-15 11:44:53 +0100

    Initialize Prueba1Backend project with Maven setup and basic configuration

---
+++ Prueba1Backend/src/test/java/com/example/Prueba1Backend/Prueba1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prueba1Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
