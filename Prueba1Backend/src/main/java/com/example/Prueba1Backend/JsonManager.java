a96b90a9752b7911b1d79e30eed359f2666c61fb
Enrique Villar <9202686@alumnos.ufv.es>
2025-01-15 12:48:30 +0100

    Edit JsonManager to use Gson for JSON handling and update vehicle retrieval method

--- Prueba1Backend/src/main/java/com/example/Prueba1Backend/JsonManager.java
+++ Prueba1Backend/src/main/java/com/example/Prueba1Backend/JsonManager.java
@@ -1,22 +1,25 @@
 package com.example.Prueba1Backend;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.type.TypeReference;
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class JsonManager {
 
-    private static final String FILE_PATH = "src/main/resources/vehicles.json"; // not sure if the path is correct, in case of error take it into account for later
-    private ObjectMapper objectMapper = new ObjectMapper();
+    public ArrayList<Vehicle> getVehicles() throws IOException {
+        String root = System.getProperty("user.dir"); // Get the root directory
+        String path = root + "/src/main/resources/vehicles.json"; // Path to the JSON file
+        String jsonContent = new String(Files.readAllBytes(Paths.get(path))); // Read the JSON file and converts all the bytes into a string
 
-    public List<Vehicle> getVehicles() throws IOException {
-        return objectMapper.readValue(new File(FILE_PATH), new TypeReference<List<Vehicle>>() {});
+        Gson gson = new Gson(); // Create a Gson object
+        Type vehicleType = new TypeToken<ArrayList<Vehicle>>(){}.getType(); // Create a Type object that represents a list of Vehicle objects
+        ArrayList<Vehicle> vehicles = gson.fromJson(jsonContent, vehicleType); // Convert the JSON content into a list of Vehicle objects
+        return vehicles;
     }
-
-    public void saveVehicles(List<Vehicle> vehicles) throws IOException {
-        objectMapper.writeValue(new File(FILE_PATH), vehicles);
-    }
+    
 }
